82162eeadfd73c9fe30292db81c2f723f56058f4
MohsenA <testnetacc125@gmail.com>
2025-09-22 18:20:11 +0330

    Add co2_emission_prediction.py.

---
+++ co2-emission-prediction/simple-linear-regression/co2_emission_prediction.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+
+df = pd.read_csv('../FuelConsumption.csv')
+cdf = df[['ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB', 'CO2EMISSIONS']]
+mask = np.random.rand(len(df)) < 0.8
+train = cdf[mask]
+test = cdf[~mask]
+
+regression = LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regression.fit(train_x, train_y)
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regression.predict(test_x)
+
+print(r2_score(test_y, test_y_))
